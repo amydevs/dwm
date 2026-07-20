234679835330d566ba9ae4742eb3b8d1e1b7f2c4
amy <ctrlalternativedelete@gmail.com>
2026-07-20 19:36:05 +1000

    feat: tokyonight theme

--- themes/working.h
+++ themes/working.h
@@ -2,5 +2,5 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#222222";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan5[]        = "#bf5d6f";
+static const char col_cyan5[]       = "#bf5d6f";
 static const char col_cyan[]        = "#bf5d6f";
