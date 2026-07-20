aebeb898fac85991f6c8c9d062d09aaec3886384
amy <ctrlalternativedelete@gmail.com>
2026-07-20 19:24:21 +1000

    fix: colours

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#222222";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#BF5D6F";
+static const char col_cyan[]        = "#bf5d6f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
